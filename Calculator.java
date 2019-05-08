52421188e22106171547074a70ed8e3ec3685d21
mumng <mumng713@gmail.com>
2019-05-08 16:34:11 +0900

    no message

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,12 @@ package com.mycompany.test;
 
 public class Calculator {
   public static void main(string[] args) {
-    
+    int a = 4;
+    int b = 2;
+    System.out.println( sum(a,b) );
+  }
+
+  private static int sum(int a, int b) {
+    return a + b;
   }
 }
